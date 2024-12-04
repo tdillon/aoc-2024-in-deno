e3406221ca0dfaf7f549717e7d78667650f9789e
Travis Dillon <tdillon25@gmail.com>
2024-12-03 19:42:15 -0500

    day 1 complete

---
+++ 1.ts
@@ -0,0 +1,13 @@
+const input = await Deno.readTextFile('input/1.txt')
+
+const x = input.trimEnd().split('\n').map(l => l.split('   ').map(a => +a))
+const y = x.map(a => a[0]).sort()
+const z = x.map(a => a[1]).sort()
+
+let sum = 0
+
+for (let i = 0; i < y.length; ++i) {
+    sum += Math.abs(y[i] - z[i])
+}
+
+console.log(sum)
---
+++ README.md
@@ -0,0 +1,14 @@
+# Advent of Code 2024 - Deno
+
+https://adventofcode.com/2024
+
+https://deno.com/blog/advent-of-code-2024
+
+
+## Dev
+
+Download https://adventofcode.com/2024/day/1/input to `input/1.txt`.
+
+```shell
+deno run --allow-read=. 1.ts
+```
